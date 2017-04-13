60fd8f45dbc8e5863bc15105db177d004a198e6b
Haaris Sohail <haarissohail@gmail.com>
2017-04-13 01:47:37 -0400

    Started working on the AsyncTask class for background processing.

---
+++ app/src/main/java/com/gmu/swe632androidproject/AsyncRouteCards.java
@@ -0,0 +1,19 @@
+package com.gmu.swe632androidproject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by haaris on 4/13/17.
+ */
+
+public class AsyncRouteCards extends AsyncTask
+{
+    private URL jsonURL;
+    private ProgressDialog progressDialog;
+    private Activity context;
+
+}
--- app/src/main/java/com/gmu/swe632androidproject/NetworkMethods.java
+++ app/src/main/java/com/gmu/swe632androidproject/NetworkMethods.java
@@ -1,5 +1,15 @@
 package com.gmu.swe632androidproject;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Created by haaris on 4/12/17.
  */
@@ -12,7 +22,82 @@ public final class NetworkMethods
     private static final String API_KEY = "AIzaSyBVGhILjQgCPzE4R_zelzcZCiGrOJ__SFM";
 
     //URL for getting only JSON data for a particular route
-    private static final String JSON_ROUTE_URL = "ttps://maps.googleapis.com/maps/api/distancematrix/json";
+    private static final String JSON_ROUTE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json";
 
+    //Parameter for units; choice between metric and imperial in user preferences
+    private static final String UNITS_PARAM = "units";
+
+    //Paramater for source location
+    private static final String SOURCE_PARAM = "origins";
+
+    //Parameter for destination
+    private static final String DEST_PARAM = "destinations";
+
+    //By default, units are imperial, since we're building this in America. But, they can be changed to metric too.
+    //A method will be written for this
+    private static String units = "imperial";
+
+    /**
+     * Builder method used to build the URL that will request JSON data via the Google Maps API.
+     * This API call only returns JSON data, which will be parsed and displayed in the CardView/RecyclerView
+     * combo that is DisplayRouteCardsActivity.
+     *
+     * @param sourceString: string representing source location
+     * @param destinationString: string representing destination location
+     * @return a URL object that represents the API call we will make a request to
+     */
+    public static URL buildCardsJSONURL (String sourceString, String destinationString)
+    {
+        Uri builtJsonUri = Uri.parse(JSON_ROUTE_URL).buildUpon().appendQueryParameter(UNITS_PARAM, units).appendQueryParameter(SOURCE_PARAM, sourceString)
+                            .appendQueryParameter(DEST_PARAM, destinationString).appendQueryParameter("key", API_KEY).build();
+
+        URL jsonURL = null;
+        try
+        {
+            jsonURL = new URL(builtJsonUri.toString());
+        }
+        catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built JSON URL = " + jsonURL);
+
+        return jsonURL;
+    }
+
+    /**
+     * This method is used to capture the JSON response from an HTTP request.
+     * This is a generic method that will be used frequently by this application
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException
+    {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try
+        {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput)
+            {
+                return scanner.next();
+            }
+            else
+            {
+                return null;
+            }
+        }
+        finally
+        {
+            urlConnection.disconnect();
+        }
+    }
 
 }
