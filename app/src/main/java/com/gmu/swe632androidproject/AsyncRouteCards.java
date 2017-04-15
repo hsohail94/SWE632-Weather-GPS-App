328f58f0f88fecaeb0b50c05c8760cc3b032aebd
Haaris Sohail <haarissohail@gmail.com>
2017-04-14 23:35:25 -0400

    Removing sensitive info from code.

--- app/src/main/java/com/gmu/swe632androidproject/AsyncRouteCards.java
+++ app/src/main/java/com/gmu/swe632androidproject/AsyncRouteCards.java
@@ -7,7 +7,10 @@ import android.support.v7.widget.RecyclerView;
 
 import org.apache.http.NameValuePair;
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -40,6 +43,34 @@ public class AsyncRouteCards extends AsyncTask <String, String, JSONArray>
 
     protected JSONArray doInBackground (String... params)
     {
+        //If there is no data passed, return null
+        if (params.length == 0)
+        {
+            return null;
+        }
+
+        //prepare request
+        String sourceLocation = params[0];
+        String destinationLocation = params[1];
+        URL jsonDataURL = NetworkMethods.buildCardsJSONURL(sourceLocation, destinationLocation);
+
+        //Try getting a JSON data response from our Maps API Call
+        try
+        {
+            //Get our JSON data from Maps, and transform it into a JSONArray for parsing purposes
+            String jsonDataResponse = NetworkMethods.getResponseFromHttpUrl(jsonDataURL);
+            JSONObject jsonDataObject = new JSONObject(jsonDataResponse);
+
+        }
+        catch (IOException e)
+        {
+
+        }
+        catch (JSONException e)
+        {
+
+        }
+
         return null;
     }
 }
--- app/src/main/java/com/gmu/swe632androidproject/NetworkMethods.java
+++ app/src/main/java/com/gmu/swe632androidproject/NetworkMethods.java
@@ -18,8 +18,8 @@ public final class NetworkMethods
 {
     private static final String TAG = NetworkMethods.class.getSimpleName(); //will be used for logcat print statements
 
-    //This is my Google API KEY for things like Maps and whatnot. PLEASE DO NOT MODIFY THIS
-    private static final String API_KEY = "AIzaSyBVGhILjQgCPzE4R_zelzcZCiGrOJ__SFM";
+    //This is the Google API KEY for things like Maps and whatnot
+    private static final String API_KEY = System.getenv("GOOGLE_API_KEY");
 
     //URL for getting only JSON data for a particular route
     private static final String JSON_ROUTE_URL = "https://maps.googleapis.com/maps/api/directions/json";
